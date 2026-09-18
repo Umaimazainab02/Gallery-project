e2b8bf89e70a085ae39d510c5194b572571d6896
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-18 21:00:47 +0500

    Added loop practice questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
   async function usedata() {
     const response = await axios(
-      "https://picsum.photos/v2/list?page=4&limit=20"
+      `https://picsum.photos/v2/list?page=${page}&limit=20`
     );
 
     console.log(response.data);
     setData(response.data);
   }
 
+  useEffect(() => {
+    usedata();
+  }, [page]);
+
   return (
     <div className="m-10">
-      <div className="flex justify-center mb-10">
-        <button
-          onClick={usedata}
-          className="bg-white text-black px-6 py-3 rounded-lg hover:bg-gray-800"
-        >
-          Click here
-        </button>
-      </div>
-
       <div className="flex flex-wrap justify-center gap-7">
         {data.map((photo) => (
           <div key={photo.id} className="w-70">
@@ -33,11 +29,32 @@ const App = () => {
               className="w-70 h-60 object-cover rounded-lg"
             />
 
-            <h2 className="text-lg font-bold mt-2 mb-2 text-white">
+            <h2 className="text-lg font-bold mt-2 mb-2">
               {photo.author}
             </h2>
           </div>
         ))}
+      </div>
+
+      <div className="flex justify-center items-center gap-4 mt-8">
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+          className="bg-amber-500 text-xl text-black py-2 px-6 disabled:opacity-50"
+        >
+          Prev
+        </button>
+
+        <h1 className="text-3xl text-white">
+          {page} 
+        </h1>
+
+        <button
+          onClick={() => setPage(page + 1)}
+          className="bg-amber-500 text-xl text-black py-2 px-6"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
